e64d3de7415d20e2443bc6e82c0e5209bc4c4734
Elana Ryznar <eryzna@gmail.com>
2019-11-16 02:04:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    student_path (@student)
   end
 
   private
